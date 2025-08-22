165f94d7ff02cb984202b8913fafe9d81546b8ec
ldn23-ct <landini1923@gmail.com>
2025-08-22 20:20:38 +0800

    change rec code

--- Reconstruction/test.py
+++ Reconstruction/test.py
@@ -3,11 +3,11 @@ import numpy as np
 def Mapping():
     '''
     input
-      pos 射线初始时刻对准零点 ndarray of shape (2,) dtype=float
-      dx/dy/dz 空间体素大小 根据空间坐标计算体素编号
-      kernal_y/kernal_z 二维卷积核尺寸 大小与体素大小相同 计算会极大简化
+      pos--射线初始时刻对准零点 ndarray of shape (2,) dtype=float
+      dx/dy/dz--空间体素大小 根据空间坐标计算体素编号
+      kernal_y/kernal_z--二维卷积核尺寸 大小与体素大小相同 计算会极大简化
     output
-      map 二维卷积核每个像素与三维空间体素的对应关系
+      map--二维卷积核每个像素与三维空间体素的对应关系
     idea
       (1) 空间体素划分编号 n = iz + iy * nz + ix * (ny + nz)
       (2) 根据 pos_x 找到 ix
@@ -17,3 +17,29 @@ def Mapping():
       (5) 当 iy 越界 map赋值null
     '''
     return
+
+def DetArray():
+    '''
+    input
+      corner_pos--4 corners position of the detector
+      dx/dy--length of the detector pixel
+      x/y--length of the detector
+    output
+      detarray--pos of the pixels' center
+    idea
+      (1) 计算像素个数
+      (2) 计算每个像素中心点
+    '''
+    return
+
+def ObjArray():
+  '''
+  input
+    angle--the angle of the fan beam
+    fdis--the distance from source to the front surface of the object
+    bdis--the distance from source to the back surface of the object
+    kernel_y/kernel_z--size of kernal, equal to voxel
+  output
+    objarray--pos of the voxels' center
+    fanmask--0/1 matrix, shape: [p, m*n], voxels which be irradiated
+  '''
