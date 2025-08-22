c361739f63dbfc5f13007bb84a33e3e5e48f3d6d
ldn23-ct <landini1923@gmail.com>
2025-08-22 16:18:22 +0800

    add Mapping

---
+++ Reconstruction/test.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def Mapping():
+    '''
+    input
+      pos 射线初始时刻对准零点 ndarray of shape (2,) dtype=float
+      dx/dy/dz 空间体素大小 根据空间坐标计算体素编号
+      kernal_y/kernal_z 二维卷积核尺寸 大小与体素大小相同 计算会极大简化
+    output
+      map 二维卷积核每个像素与三维空间体素的对应关系
+    idea
+      (1) 空间体素划分编号 n = iz + iy * nz + ix * (ny + nz)
+      (2) 根据 pos_x 找到 ix
+      (3) 假设零点是对准的 kernal相当于三维矩阵的一个切片 只需要根据 pos_y 就可以求出 iy0
+          并顺便取出每一行对应的 iy 
+      (4) 当 pos_x 位于交界线上 归 x- 像素
+      (5) 当 iy 越界 map赋值null
+    '''
+    return
