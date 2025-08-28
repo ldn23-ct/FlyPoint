0c2f57ad79b4d11282f3da2fbefaae3a5e76babd
ldn23-ct <landini1923@gmail.com>
2025-08-28 21:13:46 +0800

    add mapping

--- Reconstruction/test.py
+++ Reconstruction/test.py
@@ -1,24 +1,44 @@
 import numpy as np
 
-def Mapping(pos: np.ndarray, voxelsize: np.ndarray, kernalsize: np.ndarray):
+def Mapping(pos: np.ndarray, objsize: np.ndarray, voxelsize: np.ndarray, kernelsize: np.ndarray):
     '''
     input
       pos--模体前表面左上角点坐标 初始时刻对准零点 ndarray of shape (2,) dtype=float
-      dx/dy/dz--空间体素大小 根据空间坐标计算体素编号
-      kernal_y/kernal_z--二维卷积核尺寸 大小与体素大小相同 计算会极大简化
+      objsize--物体尺寸 ndarray of shape (3,)
+      voxelsize--空间体素大小 根据空间坐标计算体素编号
+      kernel_y/kernel_z--二维卷积核尺寸 大小与体素大小相同 计算会极大简化
     output
-      map--二维卷积核每个像素与三维空间体素的对应关系
+      n_grid--二维卷积核每个像素与三维空间体素的对应关系 shape (ky, kz)
     idea
-      (1) 空间体素划分编号 n = iz + iy * nz + ix * (ny + nz)
-      (2) 根据 pos_x 找到 ix
-      (3) 假设零点是对准的 kernal相当于三维矩阵的一个切片 只需要根据 pos_y 就可以求出 iy0
+      (1) 空间体素划分编号 n = iz + iy * nz + ix * (ny * nz)
+      (2) 零点按模体中心来计算
+      (3) 假设零点是对准的 kernel相当于三维矩阵的一个切片 只需要根据 pos_y 就可以求出 iy0
           并顺便取出每一行对应的 iy 
-      (4) 当 pos_x 位于交界线上 归 x- 像素
-      (5) 当 iy 越界 map赋值null
+      (4) 当 pos_x 位于交界线上 归 x+ 像素
+      (5) 越界 n_grid赋值-1
     '''
-    ix = -pos[0] // voxelsize[0]
+    x, y = pos[0], pos[1]
+    Lx, Ly, Lz = objsize[0], objsize[1], objsize[2]
+    dx, dy, dz = voxelsize[0], voxelsize[1], voxelsize[2]
+    ky, kz = kernelsize[0], kernelsize[1]
+    nx, ny, nz = Lx/dx, Ly/dy, Lz/dz
 
-    return
+    idx = int(np.floor((x + Lx/2) / dx))
+    if idx < 0 or idx > nx:
+        return -np.ones((ky*kz,), dtype=np.int64)
+    if idx == nx: idx = idx - 1
+
+    idy_center = int(np.floor((Ly/2 - y) / dy))
+    idy_grid = idy_center + (np.arange(ny) - ky // 2)[:, None]
+    idz_grid = np.arange(nz)[None, :]
+    in_y = (idy_grid >= 0) & (idy_grid < ny)
+    in_z = (idz_grid >= 0) & (idz_grid < nz)
+    mask = in_y & in_z
+
+    n_grid = idz_grid + idy_grid * nz + idx * (ny * nz)
+    n_grid = np.where(mask, n_grid, -1)
+
+    return n_grid
 
 def DetArray(corners: list, pixelsize: list, detsize: list):
     '''
@@ -55,28 +75,6 @@ def DetArray(corners: list, pixelsize: list, detsize: list):
     centers = np.array(centers).reshape(ny, nx)
 
     return centers
-
-def ObjArray(angle, fdis, bdis, kernalsize):
-  '''
-  input
-    angle--the angle of the fan beam
-    fdis--the distance from source to the front surface of the object
-    bdis--the distance from source to the back surface of the object
-    kernel_y/kernel_z--size of kernal, equal to voxel
-  output
-    objarray--pos of the voxels' center
-  '''
-  angle = np.deg2rad(angle)
-  height = bdis * np.tan(angle/2)
-  ny = np.ceil(height / kernalsize[0])
-  nz = np.ceil((bdis - fdis) / kernalsize[1])
-  centers = np.zeros((ny*nz, 2))
-  y0, z0 = height - kernalsize[0]/2, fdis + kernalsize[1]/2
-  for i in range(ny):
-      for j in range(nz):
-          idx = j + i * nz
-          centers[idx] = [y0 - i * kernalsize[0], z0 + j * kernalsize[1]]
-  return centers
 
 class HalfSpaceCutting:
     '''
